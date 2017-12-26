0d9a68f347dd56731663a7a658c71d94c7d9e5b9
brycefrank <bryce.frank@oregonstate.edu>
2017-12-26 09:15:11 -0700

    Adding to sample

--- R/eq.R
+++ R/eq.R
@@ -113,47 +113,4 @@ get_coefs <- function(region, spcd) {
   }
 }
 
-#' Serves as the default volume calculation function,
-#' retrieves the coefficient table for the specified species,
-#' region and volume type, generates the function, and applies
-#' it to the input data.
-#'
-#' @param dbh A column of diameter at breast height values (or a single value)
-#' @param ht A column of height values (or a single value)
-#' @param spcd FIA species code
-#' @param region Geographic region
-#' @export
-calc_cvts <- function(dbh, ht, region, spcd) {
-  # Get unique region and scd combinations
-  uniques <- unique(data.frame(region, spcd))
-
-  # Build the equations
-  uniques$eqs <- mapply(build_equation, uniques$region, uniques$spcd)
-
-  # Split the data into spcd - region groups
-  tree_data <- data.frame(dbh, ht, region, spcd)
-  tree_split <- split(tree_data, list(tree_data$region, tree_data$spcd))
-
-  new_tree <- data.frame()
-  for (group in tree_split) {
-    # Get the 'group key'
-    region <- group$region[1]
-    spcd <- group$spcd[1]
-
-    # Get the equation from eqs
-    eq <- uniques$eqs[which((uniques$spcd == spcd & uniques$region == region))]
-    eq <- eq[[1]]
-    # Apply the equation to each record in the group
-    #print(typeof(eq))
-    if(typeof(eq) == "closure") {
-      group$cvts <- mapply(eq, group$dbh, group$ht)
-    } else {
-      group$cvts <- NA
-    }
-
-    new_tree <- rbind(new_tree ,group)
-  }
-
-  return(new_tree)
-}
 
--- R/helper.R
+++ R/helper.R
@@ -16,3 +16,24 @@ find_CSV <- function(regexp) {
   return(file.path(csv_path, csv))
 }
 
+lookup_citation <- function(equation_id) {
+  # TODO At some point this will need to incorporate more
+  # than just the FIA config csv's as its scope, so it may
+  # be worth making our own citation lookup csv. This is at
+  # least a good start on that path
+
+  # Find all equation configurations
+  csv_list <- find_CSV('_eq.csv')
+
+  csv_combined <- data.frame()
+ 
+  for (csv in csv_list) {
+    csv_rd <- read.csv(csv)
+    csv_rd <- csv_rd[c(1, 3)]
+    csv_combined <- rbind(csv_combined, csv_rd)
+  }
+
+  eq_src <- csv_combined$Source[which(csv_combined$CF_VOL_EQ == equation_id)]
+
+  print(as.character(eq_src))
+}
